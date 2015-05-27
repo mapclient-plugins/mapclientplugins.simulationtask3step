b142ff957d7971bcf3ee693c24a0dcbdcf34788d
David Nickerson <nickerso@users.sf.net>
2015-05-27 23:10:23 +1200

    initial template step from wizard

---
+++ mapclientplugins/simulationtask3step/step.py
@@ -0,0 +1,102 @@
+
+'''
+MAP Client Plugin Step
+'''
+import json
+
+from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
+from mapclientplugins.simulationtask3step.configuredialog import ConfigureDialog
+
+
+class SimulationTask3Step(WorkflowStepMountPoint):
+    '''
+    Skeleton step which is intended to be a helpful starting point
+    for new steps.
+    '''
+
+    def __init__(self, location):
+        super(SimulationTask3Step, self).__init__('Simulation Task 3', location)
+        self._configured = False # A step cannot be executed until it has been configured.
+        self._category = 'Integration'
+        # Add any other initialisation code here:
+        # Ports:
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'sedml'))
+        # Port data:
+        self._portData0 = None # sedml
+        # Config:
+        self._config = {}
+        self._config['identifier'] = ''
+
+
+    def execute(self):
+        '''
+        Add your code here that will kick off the execution of the step.
+        Make sure you call the _doneExecution() method when finished.  This method
+        may be connected up to a button in a widget for example.
+        '''
+        # Put your execute step code here before calling the '_doneExecution' method.
+        self._doneExecution()
+
+    def setPortData(self, index, dataIn):
+        '''
+        Add your code here that will set the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        uses port for this step then the index can be ignored.
+        '''
+        self._portData0 = dataIn # sedml
+
+    def configure(self):
+        '''
+        This function will be called when the configure icon on the step is
+        clicked.  It is appropriate to display a configuration dialog at this
+        time.  If the conditions for the configuration of this step are complete
+        then set:
+            self._configured = True
+        '''
+        dlg = ConfigureDialog()
+        dlg.identifierOccursCount = self._identifierOccursCount
+        dlg.setConfig(self._config)
+        dlg.validate()
+        dlg.setModal(True)
+        
+        if dlg.exec_():
+            self._config = dlg.getConfig()
+        
+        self._configured = dlg.validate()
+        self._configuredObserver()
+
+    def getIdentifier(self):
+        '''
+        The identifier is a string that must be unique within a workflow.
+        '''
+        return self._config['identifier']
+
+    def setIdentifier(self, identifier):
+        '''
+        The framework will set the identifier for this step when it is loaded.
+        '''
+        self._config['identifier'] = identifier
+
+    def serialize(self):
+        '''
+        Add code to serialize this step to string.  This method should
+        implement the opposite of 'deserialize'.
+        '''
+        return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+    def deserialize(self, string):
+        '''
+        Add code to deserialize this step from string.  This method should
+        implement the opposite of 'serialize'.
+        '''
+        self._config.update(json.loads(string))
+
+        d = ConfigureDialog()
+        d.identifierOccursCount = self._identifierOccursCount
+        d.setConfig(self._config)
+        self._configured = d.validate()
+
+
